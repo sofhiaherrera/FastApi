8329af95a808c43445ff265ed0d1362ba4e1daf7
Sofhia <lauraherrreravsj@gmail.com>
2026-06-26 13:45:05 -0500

    facturas: migrar listar y crear factura a SQLModel con sesion de BD

--- app/enrutadores/facturas.py
+++ app/enrutadores/facturas.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, HTTPException, status
 from modelos.facturas import Factura, FacturaCrear, FacturaEditar
 from modelos.clientes import Cliente
 from listas import lista_facturas, lista_clientes
+from conexion_bd import Sesion_dependencia
+from sqlmodel import select
 
 rutas_facturas =APIRouter()
 
@@ -10,7 +12,10 @@ rutas_facturas =APIRouter()
 
 
 @rutas_facturas.get("/facturas", response_model=list[Factura])
-async def listar_facturas():
+async def listar_facturas(sesion: Sesion_dependencia):
+    #select * from factura
+    consulta = select(Factura)
+    lista_facturas = sesion.exec(consulta).all()
     return lista_facturas
 
 
@@ -26,13 +31,12 @@ async def listar_factura(factura_id: int):
     )
 
 
+
 @rutas_facturas.post("/facturas/{cliente_id}", response_model= Factura)
-async def crear_factura(cliente_id: int, datos_factura: FacturaCrear):
-    #buscar el cliente
-    cliente_encontrado = None 
-    for cliente in lista_clientes:
-        if cliente.id == cliente_id:
-            cliente_encontrado = cliente
+async def crear_factura(cliente_id: int, datos_factura: FacturaCrear, sesion: Sesion_dependencia):
+    #buscar el cliente en bd
+
+    cliente_encontrado = sesion.get(Cliente, cliente_id) 
     #mensaje si no existe el cliente
     if not cliente_encontrado:
         raise HTTPException(
@@ -40,13 +44,18 @@ async def crear_factura(cliente_id: int, datos_factura: FacturaCrear):
             detail=f"El cliente con id {cliente_id}, no existe"
         )
     
-    #validar datos de factura
-    factura_val = Factura.model_validate(datos_factura.model_dump())
-    factura_val.cliente = cliente_encontrado
-    #id de la factura
-    factura_val.id = len(lista_facturas)+1
-    lista_facturas.append(factura_val)
+
+    #validar datos de factura-json, pasar dict
+    factura_dict = datos_factura.model_dump()
+    factura_dict["cliente_id"] = cliente_id
+    factura_val = Factura.model_validate(factura_dict)
+    
+    #guardar en bd
+    sesion.add(factura_val)
+    sesion.commit()
+    sesion.refresh(factura_val)
     return factura_val
+
 #
 @rutas_facturas.patch("/facturas/{id_factura}", response_model= Factura)
 async def editar_factura(id_factura: int, datos_factura: FacturaEditar):
